f7be8b5572c0722e1a63c9a52e0d252c28a97a90
Gabriel Quintino <pikachurando@pm.me>
2022-03-07 19:48:12 -0300

    bugfix: fixed vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const isDevEnv = mode === 'development'
-  
-  return {
-    plugins: [
-      isDevEnv && react(),
-    ]
-  }
+export default defineConfig({
+  plugins: [react()]
 })
